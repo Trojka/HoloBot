ad23f6aaa18b226634a8df6df12258b75eed0dfa
Serge Desmedt <sergedesmedt@yahoo.com>
2018-07-17 21:21:10 +0200

    stepper motor test

---
+++ Arduino/StepperTest/StepperTest.ino
@@ -0,0 +1,25 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 1500;  // change this to fit the number of steps per revolution
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup() {
+  // set the speed at 20 rpm:
+  myStepper.setSpeed(20);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
